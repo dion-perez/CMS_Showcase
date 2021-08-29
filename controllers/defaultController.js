6da1f18d253755d1a9615b892396ec1838cdd8e9
Dion Perez <dionperez227@gmail.com>
2021-08-29 16:15:13 +0100

    Stylesheets properly added and referenced. Site formatting finally working. Looks like footer links to external sheets might not have been working.
    Register route added. Register Get/Post added.
    Register page added.

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,7 +1,23 @@
+const {response} = require("express");
 module.exports = {
     index: (req, res) => {
         res.render('default/index.handlebars');
     },
 
+    loginGet: (req, res) => {
+        res.render('default/login');
+    },
+
+    loginPost: (req, res) => {
+        res.send("Login hit, data POST.")
+    },
+
+    registerGet: (req, res) => {
+        res.render ('default/register');
+    },
+
+    registerPost: (req, res) => {
+        res.send("Successfully registered");
+    }
 
 }
